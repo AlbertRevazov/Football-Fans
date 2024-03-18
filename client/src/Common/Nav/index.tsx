4c3c44ce0bb9250eabc0bd56c500502d104a9f04
Albert <abeiphone@bk.ru>
2024-03-18 15:53:20 +0300

    draft main

--- client/src/Common/Nav/index.tsx
+++ client/src/Common/Nav/index.tsx
@@ -1,41 +1,38 @@
 import { FC, useEffect } from "react";
 import { useAppDispatch, useAppSelector } from "@/redux/hooks";
 import { getMe, logout } from "@/redux/features/AuthSlice";
+import { Links } from "@/data";
+import { useRouter } from "next/router";
 import Link from "next/link";
 import styles from "./Nav.module.scss";
 
 export const Nav: FC = () => {
   const dispatch = useAppDispatch();
   const { user } = useAppSelector((state) => state.auth);
+  const router = useRouter();
 
   useEffect(() => {
     dispatch(getMe());
   }, []);
 
   return (
-    <section style={{ borderBottom: "2px solid #000"}}>
+    <section className={styles.nav_section}>
       <nav className={styles.nav_root}>
         <Link href="/" className={styles.logo}>
-          <img
-            src="/img/logo.png"
-            width={100}
-            height={100}
-            className={styles.logo_img}
-          />
+          <img src="/svg/logo.svg" width={80} className={styles.logo_img} />
         </Link>
         <ul className={styles.ul}>
-          <Link href="/matches">
-            <li className={styles.li}>Matches</li>
-          </Link>
-          <Link href="/teams">
-            <li className={styles.li}>Teams</li>
-          </Link>
-          <Link href="/competitions">
-            <li className={styles.li}>Competitions</li>
-          </Link>
-          <Link href="/players">
-            <li className={styles.li}>Players</li>
-          </Link>
+          {Links.map((link) => {
+            const isActiveUrl = link.url === router.pathname;
+            return (
+              <Link key={link.id} href={link.url}>
+                <li className={styles[isActiveUrl ? "li_active" : "li"]}>
+                  {link.title}
+                </li>
+              </Link>
+            );
+          })}
+
           {user ? (
             <span className={styles.btn} onClick={() => dispatch(logout())}>
               EXIT
--- client/src/Components/Main/index.tsx
+++ client/src/Components/Main/index.tsx
@@ -1,7 +1,5 @@
 import React, { FC } from "react";
-import { Image } from "@/Common/UI/Image";
 import styles from "./Main.module.scss";
-import { Slider } from "@/Common/Slider";
 
 export const Main: FC = () => {
   return (
@@ -16,27 +14,6 @@ export const Main: FC = () => {
               ведь футбол – это не просто спорт, это целый мир, полный эмоций,
               страсти и соперничества.
             </p>
-          </div>
-          <div className={styles.main_image}>
-            {/* <Image
-              src="/img/welc-back.jpg"
-              alt="background pitch"
-
-              style={{ objectFit: "cover", borderRadius: "10px" }}
-            /> */}
-          </div>
-          <div className={styles.main_description}>
-            <h2>Мы здесь, чтобы помочь болельщикам по всему миру.</h2>
-            <h4>
-              На нашем сайте вы найдете все, что нужно, чтобы быть в курсе
-              последних новостей, результатов и статистики футбольных матчей.
-              <br />
-              <br />
-              Мы тщательно следим за развитием событий футбольного мира и
-              предлагаем вам самую актуальную информацию, доступную на одной
-              платформе. Наша команда живет и дышит футболом, и мы делимся этой
-              страстью с вами.
-            </h4>
           </div>
         </section>
       </div>
