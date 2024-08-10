84bc2e69ae0ec3f090f7e8220be4a92624378c66
Albert <abeiphone@bk.ru>
2024-08-10 16:07:21 +0300

    Create WelcomeSection in Main page

--- client/src/Components/Main/Main.tsx
+++ client/src/Components/Main/Main.tsx
@@ -1,21 +1,11 @@
 import React, { FC } from 'react';
 import styles from './Main.module.scss';
+import WelcomeSection from './WelcomeSection';
 
 const Main: FC = () => {
   return (
     <div className={styles.root}>
-      <div className={styles.container}>
-        <article className={styles.about}>
-          <div className={styles.titleWrap}>
-            <p className={styles.title}>football fans</p>
-            <p className={styles.subtitle}>
-              это площадка, которая объединяет настоящих любителей футбола. Наш сайт предназначен
-              для всех, кому интересна эта увлекательная игра, ведь футбол – это не просто спорт,
-              это целый мир, полный эмоций, страсти и соперничества.
-            </p>
-          </div>
-        </article>
-      </div>
+      <WelcomeSection />
     </div>
   );
 };
---
+++ client/src/Components/Main/WelcomeSection/WelcomeSection.tsx
@@ -0,0 +1,30 @@
+import React, { FC } from 'react';
+import styles from './WelcomeSection.module.scss';
+
+const WelcomeSection: FC = () => {
+  return (
+    <div className={styles.root}>
+      <article className={styles.content}>
+        <section className={styles.section}>
+          <header className={styles.header}>
+            <h2 className={styles.title}>Your Ultimate Football Companion</h2>
+            <h4 className={styles.subtitle}>
+              From match highlights to player stats, we've got all the action covered.
+            </h4>
+          </header>
+          <button className={styles.button}>Dive In</button>
+        </section>
+        <figure className={styles.figure}>
+          <img
+            src="/img/main/welcome.webp"
+            alt="Newest collection"
+            loading="lazy"
+            className={styles.firstImage}
+          />
+        </figure>
+      </article>
+    </div>
+  );
+};
+
+export default WelcomeSection;
