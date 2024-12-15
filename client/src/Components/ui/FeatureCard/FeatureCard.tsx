f7e0abfe065b3c1695794234e3076425486b9954
Albert <abeiphone@bk.ru>
2024-12-15 20:50:20 +0300

    new styles for Feature Card

---
+++ client/src/Components/ui/FeatureCard/FeatureCard.tsx
@@ -0,0 +1,21 @@
+import React, { FC } from 'react';
+import styles from './feature-card.module.scss';
+
+interface IFeatureCardProps {
+  feature: { id: number; title: string; description: string; icon: string };
+}
+
+const FeatureCard: FC<IFeatureCardProps> = ({ feature }) => {
+  return (
+    <div className={styles.featureCardContainer}>
+      <article className={styles.feature}>
+        <h3 className={styles.title}>{feature.title}</h3>
+      </article>
+      <article className={styles.featureBack}>
+        <p className={styles.description}>{feature.description}</p>
+      </article>
+    </div>
+  );
+};
+
+export default FeatureCard;
